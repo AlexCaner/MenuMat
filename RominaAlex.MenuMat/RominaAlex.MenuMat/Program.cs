97224d672abfb15e55726ab3b438dc8eda469883
Alex Caner <a2308752@institutmontilivi.cat>
2024-01-15 11:41:10 +0100

    15/01/2024 segona pujada

---
+++ RominaAlex.MenuMat/RominaAlex.MenuMat/Program.cs
@@ -0,0 +1,39 @@
+﻿int opcio, num1, num2;
+opcio = Console.WriteLine(Menu(opcio));
+
+switch (opcio)
+{
+    case 1:
+        Console.WriteLine("Escriu un numero");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Escriu un numero");
+        num2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(MCD(num1, num2));
+        break;
+}
+
+//MENU
+static int Menu(int opcio)
+{
+    Console.WriteLine("Que vols fer");
+    int opcio = Convert.ToInt32(Console.ReadLine());
+    return opcio;
+
+};
+
+
+//MCD
+
+static void MCD(int num1, int num2)
+{
+
+    while (num2 != 0)
+    {
+        int temp = num2;
+        num2 = num1 % num2;
+        num1 = temp;
+    }
+
+    Console.WriteLine(num1);
+}
