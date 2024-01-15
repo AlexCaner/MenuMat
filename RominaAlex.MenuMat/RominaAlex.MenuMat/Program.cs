9b40c0507e3a3ad4ca1346b0ab13a9025d341320
Alex Caner <a2308752@institutmontilivi.cat>
2024-01-15 13:12:43 +0100

    15/01/2024 Acabat la clase, falta acabar algunes de les operacions i el diseny de interficie del menu

--- RominaAlex.MenuMat/RominaAlex.MenuMat/Program.cs
+++ RominaAlex.MenuMat/RominaAlex.MenuMat/Program.cs
@@ -1,31 +1,81 @@
-﻿int opcio, num1, num2;
-opcio = Console.WriteLine(Menu(opcio));
+﻿using System.Net.Http.Headers;
+using System.Threading;
 
-switch (opcio)
-{
-    case 1:
-        Console.WriteLine("Escriu un numero");
-        num1 = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine("Escriu un numero");
-        num2 = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine(MCD(num1, num2));
-        break;
-}
+int opcio = 1;
 
 //MENU
-static int Menu(int opcio)
+while(opcio > 0 && opcio < 9)
 {
-    Console.WriteLine("Que vols fer");
-    int opcio = Convert.ToInt32(Console.ReadLine());
-    return opcio;
+    Console.Write(" 0. Sortir\n 1. Màxim\n 2. MCD\n 3. MCM\n 4. Factorial \n 5. Combinatori\n 6. Mostrar Divisor Major\n 7. Es Primer\n 8. Primers Numeros Primers\n");
+    Console.Write("Quina Opcio vols?: ");
+    opcio = Convert.ToInt32(Console.ReadLine());
+    Console.Clear();
+    Switch(opcio);
+}
+Console.WriteLine("Adeu");
 
-};
+//METODES
+//SWITCH QUE BUSCA LA OPCIO
 
+static void Switch(int opcio){ 
+switch (opcio)
+{
+    case 0:
+        opcio = 0; 
+        break;
+
+    case 1:
+        int num1 = IntroduirValor();
+        int num2 = IntroduirValor();
+        int resultat = Maxim(num1, num2);
+        Console.WriteLine($"El resultat es: {resultat}");
+        Thread.Sleep(4000);
+        Console.Clear();
+        break;
+
+    case 2:
+        num1 = IntroduirValor();
+
+        num2 = IntroduirValor();
+
+        resultat = MCD(num1, num2);
+        Console.WriteLine($"El resultat es: {resultat}");
+        Thread.Sleep(4000);
+        Console.Clear();
+        break;
+
+    case 7:
+        num1 = IntroduirValor();
+        bool Primer = EsPrimer(num1);
+        Console.WriteLine($"El resultat es: {Primer}");
+        Thread.Sleep(4000);
+        Console.Clear();
+        break;
+    }
+}
+
+
+//INTRODUIR VALOR
+static int IntroduirValor()
+{
+    Console.Write("Escriu el valor: ");
+    int valor = Convert.ToInt32(Console.ReadLine());
+    Console.Clear();
+    return valor;
+}
+//Maxim
+static int Maxim(int num1, int num2)
+{
+    int res = 0;
+
+    if (num1 > num2) res = num1;
+    else res = num2;
+    return res;
+}
 
 //MCD
 
-static void MCD(int num1, int num2)
+static int MCD(int num1, int num2)
 {
 
     while (num2 != 0)
@@ -35,5 +85,22 @@ static void MCD(int num1, int num2)
         num1 = temp;
     }
 
-    Console.WriteLine(num1);
+    return num1;
+}
+
+//EsPrimer
+static bool EsPrimer(int num1)
+{
+    bool res = false;
+
+    int i = 2, divisor = 0;
+
+    while (i <= num1)
+    {
+        if (num1 % i == 0)
+            divisor++;
+        i++;
+    }
+    if (divisor == 1) res = true;
+    return res;
 }
