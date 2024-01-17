81416cffbf75c96da66c503feea98771608923a2
Alex Caner <a2308752@institutmontilivi.cat>
2024-01-17 12:42:28 +0100

    17/01/2024 Backup

--- RominaAlex.MenuMat/RominaAlex.MenuMat/Program.cs
+++ RominaAlex.MenuMat/RominaAlex.MenuMat/Program.cs
@@ -5,7 +5,7 @@ class Program
 {
     static void Main()
     {
-        int opcio = 0;
+        int opcio = 1;
 
         while (true)
         {
@@ -30,6 +30,22 @@ class Program
                     else opcio = 1;
                    
                     break;
+                case ConsoleKey.LeftArrow:
+                    if (opcio > 1)
+                    {
+                        opcio = opcio - 1;
+                    }
+                    else opcio = 9;
+
+                    break;
+                case ConsoleKey.RightArrow:
+                    if (opcio < 9)
+                    {
+                        opcio = opcio + 1;
+                    }
+                    else opcio = 1;
+
+                    break;
                 case ConsoleKey.Enter:
                     Switch(opcio);
                     break;
@@ -48,13 +64,13 @@ class Program
             if (i == opcio)
             {
                 Console.BackgroundColor = ConsoleColor.DarkMagenta;
-                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
             }
 
             switch (i)
             {
                 case 1:
-                    Console.WriteLine("││ 1. Màxim                        ││");
+                    Console.WriteLine($"││ 1. Màxim                        ││");
                     break;
                 case 2:
                     Console.WriteLine("││ 2. MCD                          ││");
@@ -96,35 +112,50 @@ class Program
         switch (opcio)
         {
             case 9:
-                Console.WriteLine("    / \\__\r\n  (    @\\___\r\n  /         O\r\n /   (_____/\r\n/_____/ADEU!");
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("<!-- __| |_________________________________________________________________| |__ -->\r\n<!-- __   _________________________________________________________________   __ -->\r\n<!--   | |                                                                 | |   -->\r\n<!--   | |                                                                 | |   -->\r\n<!--   | |                                                                 | |   -->\r\n<!--   | |            d8888 8888888b.  8888888888 888     888 888          | |   -->\r\n<!--   | |           d88888 888  \"Y88b 888        888     888 888          | |   -->\r\n<!--   | |          d88P888 888    888 888        888     888 888          | |   -->\r\n<!--   | |         d88P 888 888    888 8888888    888     888 888          | |   -->\r\n<!--   | |        d88P  888 888    888 888        888     888 888          | |   -->\r\n<!--   | |       d88P   888 888    888 888        888     888 Y8P          | |   -->\r\n<!--   | |      d8888888888 888  .d88P 888        Y88b. .d88P  \"           | |   -->\r\n<!--   | |     d88P     888 8888888P\"  8888888888  \"Y88888P\"  888          | |   -->\r\n<!--   | |                                                                 | |   -->\r\n<!--   | |                                                                 | |   -->\r\n<!-- __| |_________________________________________________________________| |__ -->\r\n<!-- __   _________________________________________________________________   __ -->\r\n<!--   | |                                                                 | |   -->");
+                Console.ResetColor();
                 Environment.Exit(0);
                 break;
             case 1:
+                Console.BackgroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine($"--------- MÀXIMS ---------");
+                Console.ResetColor();
                 int num1 = IntroduirValor();
                 int num2 = IntroduirValor();
                 int resultat = Maxim(num1, num2);
+                Console.BackgroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine($"\n-------- RESULTAT --------");
+                Console.ResetColor();
                 Console.WriteLine($"El número més gran es {resultat}");
                 Thread.Sleep(4000);
                 Console.Clear();
                 break;
             case 2:
+                Console.BackgroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine($"----------- MCD -----------");
+                Console.ResetColor();
                 num1 = IntroduirValor();
                 num2 = IntroduirValor();
                 resultat = MCD(num1, num2);
+                Console.BackgroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine($"\n-------- RESULTAT --------");
+                Console.ResetColor();
                 Console.WriteLine($"El resultat es: {resultat}");
                 Thread.Sleep(4000);
                 Console.Clear();
                 break;
             case 7:
-                Console.WriteLine($"----------- MCM -----------");
+                Console.BackgroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine($"---------- PRIMER ----------");
+                Console.ResetColor();
                 num1 = IntroduirValor();
                 bool Primer = EsPrimer(num1);
+                Console.BackgroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine($"\n-------- RESULTAT --------");
-                Console.WriteLine($"El resultat es: {Primer}");
+                Console.ResetColor();
+                if (Primer) Console.WriteLine("Si es un número primer");
+                else Console.WriteLine("No es un número primer");
                 Thread.Sleep(4000);
                 Console.Clear();
                 break;
@@ -177,111 +208,3 @@ class Program
     }
     
 }
-
-
-/*using System.Net.Http.Headers;
-using System.Threading;
-
-int opcio = 1;
-
-//MENU
-while(opcio > 0 && opcio < 9)
-{
-    Console.Write(" 0. Sortir\n 1. Màxim\n 2. MCD\n 3. MCM\n 4. Factorial \n 5. Combinatori\n 6. Mostrar Divisor Major\n 7. Es Primer\n 8. Primers Numeros Primers\n");
-    Console.Write("Quina Opcio vols?: ");
-    opcio = Convert.ToInt32(Console.ReadLine());
-    Console.Clear();
-    Switch(opcio);
-}
-Console.WriteLine("Adeu");
-
-//METODES
-//SWITCH QUE BUSCA LA OPCIO
-
-static void Switch(int opcio){ 
-switch (opcio)
-{
-    case 0:
-        opcio = 0; 
-        break;
-
-    case 1:
-        int num1 = IntroduirValor();
-        int num2 = IntroduirValor();
-        int resultat = Maxim(num1, num2);
-        Console.WriteLine($"El resultat es: {resultat}");
-        Thread.Sleep(4000);
-        Console.Clear();
-        break;
-
-    case 2:
-        num1 = IntroduirValor();
-
-        num2 = IntroduirValor();
-
-        resultat = MCD(num1, num2);
-        Console.WriteLine($"El resultat es: {resultat}");
-        Thread.Sleep(4000);
-        Console.Clear();
-        break;
-
-    case 7:
-        num1 = IntroduirValor();
-        bool Primer = EsPrimer(num1);
-        Console.WriteLine($"El resultat es: {Primer}");
-        Thread.Sleep(4000);
-        Console.Clear();
-        break;
-    }
-}
-
-
-//INTRODUIR VALOR
-static int IntroduirValor()
-{
-    Console.Write("Escriu el valor: ");
-    int valor = Convert.ToInt32(Console.ReadLine());
-    return valor;
-}
-//Maxim
-static int Maxim(int num1, int num2)
-{
-    int res = 0;
-
-    if (num1 > num2) res = num1;
-    else res = num2;
-    return res;
-}
-
-//MCD
-
-static int MCD(int num1, int num2)
-{
-
-    while (num2 != 0)
-    {
-        int temp = num2;
-        num2 = num1 % num2;
-        num1 = temp;
-    }
-
-    return num1;
-}
-
-//EsPrimer
-static bool EsPrimer(int num1)
-{
-    bool res = false;
-
-    int i = 2, divisor = 0;
-
-    while (i <= num1)
-    {
-        if (num1 % i == 0)
-            divisor++;
-        i++;
-    }
-    if (divisor == 1) res = true;
-    return res;
-}
-*/
